4f23c134e30a0cfc6c220388f2a381685ffce1d6
Chris Tankersley <dragonmantank@gmail.com>
2010-12-21 16:38:27 -0500

    Resource manager now handles namespaces for resource locations

--- library/Pillar/Rest/Resource/Manager.php
+++ library/Pillar/Rest/Resource/Manager.php
@@ -1,6 +1,6 @@
 <?php
 
-/** 
+/**
  * Used to find and create resource objects
  * Used to detect when no resource was specified
  *
@@ -10,6 +10,22 @@
  */
 class Pillar_Rest_Resource_Manager
 {
+    /**
+     * Namespaces for Pillar to append to resource names
+     * @var array
+     */
+    protected static $namespaces = array();
+
+    /**
+     * Adds a namespace into the stack
+     * @param string $class
+     * @param string $path
+     */
+    public static function addNamespace($class, $path)
+    {
+        static::$namespaces[$class] = $path;
+    }
+
     /**
      * Returns a Resource object
      * @param mixed $resource
@@ -21,32 +37,25 @@ class Pillar_Rest_Resource_Manager
         if($resource instanceof Pillar_Rest_Request) {
             $resource = static::getResourceName($resource);
         }
-        
-        $resource = ucfirst($resource);
-        $name = 'Resource_'.$resource;
-        $filename = 'Resource/'.$resource.'.php';
 
-        if(static::fileExists($filename)) {
-            $class = new $name;
-            return $class;
+        $resource = ucfirst($resource);
+
+        $class = static::getResourceClass($resource);
+        if($class !== null) {
+            $object = new $class();
+            return $object;
         }
 
         throw new Pillar_Rest_Exception_NonexistantResource('Resource '.$resource.' does not exist');
     }
 
-    public static function fileExists($filename)
+    /**
+     * Returns the currently registered namespaces
+     * @return array
+     */
+    public static function getNamespaces()
     {
-        $paths = explode(PATH_SEPARATOR, get_include_path());
-        foreach ($paths as $path) {
-            if (substr($path, -1) == DIRECTORY_SEPARATOR) {
-                $fullpath = $path.$filename;
-            } else {
-                $fullpath = $path.DIRECTORY_SEPARATOR.$filename;
-            }
-            if (file_exists($fullpath)) {
-                return $fullpath;
-            }
-        }
+        return static::$namespaces;
     }
 
     /**
@@ -60,5 +69,35 @@ class Pillar_Rest_Resource_Manager
         list($name) = explode('/', $name);
 
         return $name;
+    }
+
+    /**
+     * Returns the full class name of a resource based upon the short name
+     * Searches through all the namespaces that have been registered, and then
+     * falls back to just looking for <name>.php . If nothing is found, then
+     * null is returned.
+     *
+     * @param string $resource
+     * @return string
+     */
+    public static function getResourceClass($resource)
+    {
+        $file = ucfirst($resource).'.php';
+
+        $searches = array();
+        foreach(static::$namespaces as $key => $value) {
+            $searches[$key] = $value.$file;
+        }
+
+        foreach(explode(PATH_SEPARATOR, get_include_path()) as $dir) {
+            foreach($searches as $key => $class) {
+                echo "Searching for $dir"."$class<br>";
+                if(is_file($dir.'/'.$class)) {
+                    return $key.'_'.ucfirst($resource);
+                }
+            }
+        }
+
+        return null;
     }
 }
